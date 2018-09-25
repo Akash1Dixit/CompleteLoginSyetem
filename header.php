b719aec8f08908b60fc6d22d87005d94462ff660
Akash Dixit <anshudixit00011@gmail.com>
2018-09-25 21:41:12 +0530

    update

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login System</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<div class="main-wrapper">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+				</ul>
+				<div class="nav-login">
+					<form action="" method="post">
+						<input type="text" name="uid" placeholder="Username/email">
+						<input type="password" name="pwd" placeholder="password">
+						<button type="submit" name="submit">Login</button>
+					</form>
+					<a href="signup.php">SignUp</a>
+				</div>
+			</div>
+		</nav>
+	</header>
--- index.php
+++ index.php
@@ -1,29 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Login System</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<header>
-		<nav>
-			<div class="main-wrapper">
-				<ul>
-					<li><a href="index.php">Home</a></li>
-				</ul>
-				<div class="nav-login">
-					<form action="" method="post">
-						<input type="text" name="uid" placeholder="Username/email">
-						<input type="password" name="pwd" placeholder="password">
-						<button type="submit" name="submit">Login</button>
-					</form>
-					<a href="signup.php">SignUp</a>
-				</div>
-			</div>
-		</nav>
-	</header>
+<?php include "header.php"; ?>
 
-  <section>
+  <section class="main-container">
+  	<div class="main-wrapper">
+  		<h2>Home</h2>
+  		
+  	</div>
   	
   </section>
 
