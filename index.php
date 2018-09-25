eb665f4cad5e36d58092a19bd80aeba911291561
Akash Dixit <anshudixit00011@gmail.com>
2018-09-25 21:01:56 +0530

    update

--- index.php
+++ index.php
@@ -2,12 +2,24 @@
 <html>
 <head>
 	<title>Login System</title>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<header>
 		<nav>
-			<div class=""></div>
+			<div class="main-wrapper">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+				</ul>
+				<div class="nav-login">
+					<form action="" method="post">
+						<input type="text" name="uid" placeholder="Username/email">
+						<input type="password" name="pwd" placeholder="password">
+						<button type="submit" name="submit">Login</button>
+					</form>
+					<a href="signup.php">SignUp</a>
+				</div>
+			</div>
 		</nav>
 	</header>
 
