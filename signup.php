c00b96c8ff6a9d816090f59742dcaf96bbf0b05e
Akash Dixit <anshudixit00011@gmail.com>
2018-09-25 22:13:01 +0530

    update

--- signup.php
+++ signup.php
@@ -3,12 +3,13 @@
   <section class="main-container">
   	<div class="main-wrapper">
   		<h2>Signup</h2>
-  		<form action="" method="post">
-  			<input type="text" name="first" placeholder="FirstName"><br />
-  			<input type="text" name="last" placeholder="LastName"><br />
-  			<input type="email" name="email" placeholder="Email"><br />
-  			<input type="text" name="uid" placeholder="UserName"><br />
-  			<input type="password" name="pwd" placeholder="password"><br />
+  		<form action="" method="post" class="signup-form">
+  			<input type="text" name="first" placeholder="FirstName">
+  	
+  			<input type="text" name="last" placeholder="LastName">
+  			<input type="email" name="email" placeholder="Email">
+  			<input type="text" name="uid" placeholder="UserName">
+  			<input type="password" name="pwd" placeholder="password">
   			<button type="submit" name="submit">Register</button>
   			
   		</form>
